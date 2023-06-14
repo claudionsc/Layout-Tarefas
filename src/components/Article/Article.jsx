d93eff412ea2e345ee6405dce6abc5263c35ec6a
alvigabriel <claudio.alves20112011@hotmail.com>
2023-06-14 09:46:25 -0300

    Finalizando componente article

--- src/components/Article/Article.jsx
+++ src/components/Article/Article.jsx
@@ -2,6 +2,34 @@ import React from 'react'
 
 export const Article = () => {
   return (
-    <article className='article'>Article</article>
+    <article className='article'>
+      <section>
+        <h1>Lista de Tarefas</h1>
+      </section>
+      
+      <section className='tarefas'>
+        <span>
+          <h2>Dia</h2>
+          <ul>
+            <li>Lorem ipsum dolor sit amet...</li>
+            <li>Lorem ipsum dolor sit amet...</li>
+            <li>Lorem ipsum dolor sit amet...</li>
+            <li>Lorem ipsum dolor sit amet...</li>
+            <li>Lorem ipsum dolor sit amet...</li>
+          </ul>
+        </span>
+        
+        <span>
+          <h2>Noite</h2>
+          <ul>
+            <li>Lorem ipsum dolor sit amet...</li>
+            <li>Lorem ipsum dolor sit amet...</li>
+            <li>Lorem ipsum dolor sit amet...</li>
+            <li>Lorem ipsum dolor sit amet...</li>
+            <li>Lorem ipsum dolor sit amet...</li>
+          </ul>
+        </span>
+      </section>
+    </article>
   )
 }
