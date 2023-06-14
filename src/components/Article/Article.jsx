3a3d57fd85d3bb64eefd2bbe8531b568a80bd3d6
alvigabriel <claudio.alves20112011@hotmail.com>
2023-06-14 08:59:03 -0300

    Finalizando componente  aside

--- src/components/Article/Article.jsx
+++ src/components/Article/Article.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export const Article = () => {
   return (
-    <article>Article</article>
+    <article className='article'>Article</article>
   )
 }
--- src/components/Aside/Aside.jsx
+++ src/components/Aside/Aside.jsx
@@ -1,7 +1,28 @@
 import React from 'react'
+import Person from '../../assets/img/person-icon-outline-2.jpg'
 
 export const Aside = () => {
-  return (
-    <aside className='aside'>Aside</aside>
-  )
+    return (
+        <aside className='aside'>
+            <section >
+                <div className="img">
+                    <img src={Person} alt="Person" />
+                </div>
+                <h1>Nome genérico</h1>
+            </section>
+
+            <section>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloribus voluptate ut a omnis nihil officia facere adipisci, iure, dolores ad repellat, earum amet? Maiores tempora iste, porro explicabo reiciendis officia!</p>
+            </section>
+
+            <section>
+                <ul>
+                    <li><a href="#">Link1</a></li>
+                    <li><a href="#">Link2</a></li>
+                    <li><a href="#">Link3</a></li>
+                    <li><a href="#">Link4</a></li>
+                </ul>
+            </section>
+        </aside>
+    )
 }
