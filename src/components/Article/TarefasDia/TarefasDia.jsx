43b9dcb48d96bac0168ad8dd20fbe73181b0438c
Claudio Nascimento <claudio.alves20112011@hotmail.com>
2023-06-14 21:01:29 -0300

    finalizando

--- src/components/Article/TarefasDia/TarefasDia.jsx
+++ src/components/Article/TarefasDia/TarefasDia.jsx
@@ -1,20 +1,13 @@
-import React, {useState, useRef} from 'react'
+import React, { useState, useRef } from 'react'
 import { LuTrash2 } from 'react-icons/lu'
-import { GrFormCheckmark } from 'react-icons/gr'
-
 
 export const TarefasDia = () => {
 
-    
-  const [listaTarefasDia, setListaTarefasDia] = useState(() => { return  localStorage.getItem("ListaDia") ? JSON.parse(localStorage.getItem("ListaDia")) : [] })
+
+  const [listaTarefasDia, setListaTarefasDia] = useState(() => { return localStorage.getItem("ListaDia") ? JSON.parse(localStorage.getItem("ListaDia")) : [] })
   const [tarefaDia, setTarefaDia] = useState(() => { return '' })
   const [horaDia, setHoraDia] = useState()
 
-  const [tarefaRealizada, setTarefaRealizada] = useState(localStorage.getItem("TarefaRealizadaDia") ? JSON.parse(localStorage.getItem("TarefaRealizadaDia")) : false)
-
-  localStorage.setItem("TarefaRealizadaDia", JSON.stringify(tarefaRealizada))
-
-  
   localStorage.setItem("ListaDia", JSON.stringify(listaTarefasDia))
 
   const id = useRef(0)
@@ -29,42 +22,49 @@ export const TarefasDia = () => {
         {
           id: id.current,
           hora: horaDia,
-          tarefa: tarefaDia
+          tarefa: tarefaDia,
         }]
       })
-
       id.current++
     }
-
-
   }
 
   const deleteTarefa = (id) => {
     const del = listaTarefasDia.filter(tarefa => tarefa.id !== id)
     setListaTarefasDia(del)
-    setTarefaRealizada(false)
   }
-    
+
+  const deletarTodasTarefas = () => {
+    localStorage.setItem("ListaDia", [])
+    setListaTarefasDia([])
+  }
+
+
   return (
     <span>
-          <h2>Dia</h2>
-          <ul>
-            {listaTarefasDia.map((item) => {
+      <h2>Dia</h2>
+      <ol>
+        {listaTarefasDia.map((item) => {
 
-              return <>
-                        <li className={`${tarefaRealizada}`} key={item.id} id={item.id}>{item.hora}: {item.tarefa}</li>
-                        <LuTrash2 onClick={() => { deleteTarefa(item.id) }} />
-                        <GrFormCheckmark onClick={() => setTarefaRealizada(true)} />
-                    </>
-            })}
-            <form action="" method="submit" >
-              <input type="time" className="hora" name="appt"
-                min="05:00" max="18:00" required onChange={(e) => { setHoraDia(e.target.value) }} />
-              <input type="text" placeholder='Adicionar tarefa' onChange={(e) => { setTarefaDia(e.target.value) }} required />
-              <input className='form-btn' type="button" value="+" onClick={handleSubmit} />
-            </form>
+          return <>
+            <li key={item.id} id={item.id}>
+              <input type="checkbox" name={item.tarefa} id={item.tarefa} />
+              <label htmlFor={item.tarefa}>{item.hora}: {item.tarefa}</label>
+              <LuTrash2 onClick={() => { deleteTarefa(item.id) }} />
+            </li>
+          </>
+        })}
+        <form action="" method="submit" >
+          <input type="time" className="hora" name="appt"
+            min="05:00" max="18:00" required onChange={(e) => { setHoraDia(e.target.value) }} />
+          <input type="text" placeholder='Adicionar tarefa' onChange={(e) => { setTarefaDia(e.target.value) }} required />
+          <input className='form-btn' type="button" value="+" onClick={handleSubmit} />
+        </form>
 
-          </ul>
-        </span>
+        <button onClick={() => deletarTodasTarefas()}>Excluir todas as tarefas</button>
+      </ol>
+
+
+    </span>
   )
 }
--- src/components/Article/TarefasNoite/TarefasNoite.jsx
+++ src/components/Article/TarefasNoite/TarefasNoite.jsx
@@ -1,18 +1,13 @@
-import React, {useState, useRef} from 'react'
+import React, { useState, useRef } from 'react'
 import { LuTrash2 } from 'react-icons/lu'
-import { GrFormCheckmark } from 'react-icons/gr'
 
 export const TarefasNoite = () => {
 
-    
-  const [listaTarefasNoite, setListaTarefasNoite] = useState(() => { return  localStorage.getItem("ListaNoite") ? JSON.parse(localStorage.getItem("ListaNoite")) : [] })
+
+  const [listaTarefasNoite, setListaTarefasNoite] = useState(() => { return localStorage.getItem("ListaNoite") ? JSON.parse(localStorage.getItem("ListaNoite")) : [] })
   const [tarefaNoite, setTarefaNoite] = useState(() => { return '' })
   const [horaNoite, setHoraNoite] = useState()
 
-  const [tarefaRealizada, setTarefaRealizada] = useState(localStorage.getItem("TarefaRealizadaNoite") ? JSON.parse(localStorage.getItem("TarefaRealizadaNoite")) : false)
-
-  localStorage.setItem("TarefaRealizadaNoite", JSON.stringify(tarefaRealizada))
-  
   localStorage.setItem("ListaNoite", JSON.stringify(listaTarefasNoite))
 
   const id = useRef(0)
@@ -34,35 +29,41 @@ export const TarefasNoite = () => {
       id.current++
     }
 
-
   }
 
   const deleteTarefa = (id) => {
     const del = listaTarefasNoite.filter(tarefa => tarefa.id !== id)
     setListaTarefasNoite(del)
-    setTarefaRealizada(false)
   }
-    
+  const deletarTodasTarefas = () => {
+    localStorage.setItem("ListaNoite", [])
+    setListaTarefasNoite([])
+
+  }
+
   return (
     <span>
-          <h2>Noite</h2>
-          <ul>
-            {listaTarefasNoite.map((item) => {
+      <h2>Noite</h2>
+      <ol>
+        {listaTarefasNoite.map((item) => {
 
-              return <>
-                        <li className={`${tarefaRealizada}`} key={item.id} id={item.id}>{item.hora}: {item.tarefa}</li>
-                        <LuTrash2 onClick={() => { deleteTarefa(item.id) }} />
-                        <GrFormCheckmark onClick={() => setTarefaRealizada(true)} />
-                    </>
-            })}
-            <form action="" method="submit" >
-              <input type="time" className="hora" name="appt"
-                min="18:00" max="05:00" required onChange={(e) => { setHoraNoite(e.target.value) }} />
-              <input type="text" placeholder='Adicionar tarefa' onChange={(e) => { setTarefaNoite(e.target.value) }} required />
-              <input className='form-btn' type="button" value="+" onClick={handleSubmit} />
-            </form>
+          return <>
+            <li key={item.id} id={item.id}>
+              <input type="checkbox" name={item.tarefa} id={item.tarefa} />
+              <label htmlFor={item.tarefa}>{item.hora}: {item.tarefa}</label>
+              <LuTrash2 onClick={() => { deleteTarefa(item.id) }} />
+            </li>
+          </>
+        })}
+        <form action="" method="submit" >
+          <input type="time" className="hora" name="appt"
+            min="18:00" max="05:00" required onChange={(e) => { setHoraNoite(e.target.value) }} />
+          <input type="text" placeholder='Adicionar tarefa' onChange={(e) => { setTarefaNoite(e.target.value) }} required />
+          <input className='form-btn' type="button" value="+" onClick={handleSubmit} />
+        </form>
+        <button onClick={() => deletarTodasTarefas()}>Excluir todas as tarefas</button>
+      </ol>
 
-          </ul>
-        </span>
+    </span>
   )
 }
